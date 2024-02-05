b34fc293140cda5a53d927ba4f224c9d2fe14208
efronpaduansi <eufrondpaduansi@gmail.com>
2024-02-05 20:47:09 +0700

    update dashboard

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -2,13 +2,23 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Models\Jabatan;
+use App\Models\Karyawan;
+use App\Models\Pinjaman;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view("adminpanel.pages.dashboard");
+        $jmlKaryawan = Karyawan::count();
+        $jmlUser = User::count();
+        $jmlPinjaman = Pinjaman::count();
+        $jmlJabatan = Jabatan::count();
+        return view("adminpanel.pages.dashboard", compact(
+            'jmlKaryawan', 'jmlUser', 'jmlPinjaman', 'jmlJabatan'
+        ));
     }
 }
